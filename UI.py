fba17901dcbe2778759b0a6d7b8beff4d751834c
DmcDante115 <l24020567@veracruz.tecnm.mx>
2026-04-07 23:34:08 -0600

    actu 3

--- UI.py
+++ UI.py
@@ -7,6 +7,7 @@ Pilar = CTkFrame(Ventana,  fg_color="#2B2B2B", corner_radius=0)
 Pilar.place( x = 0, y = 40, relwidth=0.105, relheight=1)
 Fila = CTkFrame(Ventana, width=3000, height=120,  fg_color="#2B2B2B", corner_radius=0)
 Fila.place( x = 0, y = 0, relwidth=1)
-
+Msj= CTkFrame (Ventana, fg_color = "blue", corner_radius=0)
+Msj.place( x = 126, y = 120, relwidth=0.985, relheight=1)
 
 Ventana.mainloop()
