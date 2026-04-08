72478cbbd923054f2cce9af682c90e1e614dd1d5
DmcDante115 <l24020567@veracruz.tecnm.mx>
2026-04-07 23:00:13 -0600

    avance ui

--- UI.py
+++ UI.py
@@ -1,6 +1,11 @@
 from customtkinter import *
 
 Ventana = CTk()
-Ventana.geometry("800x700+450+100")
+Ventana.geometry("1200x700+250+50")
+Pilar = CTkFrame(Ventana, width=100, height=1000,  fg_color="#2B2B2B")
+Pilar.place( relx = 0, rely = 0)
+
+Fila = CTkFrame(Ventana, width=3000, height=100,  fg_color="#2B2B2B")
+Fila.place( relx = 0, rely = 0)
 
 Ventana.mainloop()
