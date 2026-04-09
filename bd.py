dba402688411223fb9db38b2d97af32ca6ebc489
AGEB2006 <l24020272@veracruz.tecnm.mx>
2026-04-09 17:03:07 -0600

    bd

--- bd.py
+++ bd.py
@@ -1,46 +1,9 @@
-import sqlite3
-
-conexion = sqlite3.connect("BaseDeDatos.db")
-cursor = conexion.cursor()
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS Correos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    correo TEXT UNIQUE NOT NULL,
-    contraseña TEXT NOT NULL,
-    nombre TEXT
-)
-               
-""")
-
-conexion.commit()
-print("Base de datos inicializada correctamente.")
-
-
-def registrar(correo, contraseña, nombre):
-    conexion = sqlite3.connect("BaseDeDatos.db")
-    cursor = conexion.cursor()
-
-    try:
-        cursor.execute("""
-        INSERT INTO Correos (correo, contraseña, nombre)
-        VALUES (?, ?, ?)
-        """, (correo, contraseña, nombre))
-
-        conexion.commit()
-        print("Usuario registrado correctamente ")
-
-    except sqlite3.IntegrityError:
-        print("Ese correo ya está registrado en la base de datos.")
-
-    conexion.close()
-
 def login(correo, contraseña):
-    conexion = sqlite3.connect("correo.db")
+    conexion = sqlite3.connect("BaseDeDatos.db")  # MISMA BD
     cursor = conexion.cursor()
 
     cursor.execute("""
-    SELECT id, contraseña FROM usuarios WHERE correo = ?
+    SELECT id, contraseña FROM Correos WHERE correo = ?
     """, (correo,))
 
     usuario = cursor.fetchone()
@@ -48,7 +11,7 @@ def login(correo, contraseña):
     if usuario:
         if usuario[1] == contraseña:
             print("Login correcto 🔥")
-            return usuario[0]  
+            return usuario[0]
         else:
             print("Contraseña incorrecta ❌")
     else:
@@ -56,9 +19,3 @@ def login(correo, contraseña):
 
     conexion.close()
     return None
-
-registrar("inge@gmail.com", "1234", "Inge")
-
-user_id = login("inge@gmail.com", "1234")
-
-print("ID del usuario:", user_id)
