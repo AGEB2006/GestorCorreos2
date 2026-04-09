4afee9be337e9fcbeae584b75e73179aab7ecfaf
AGEB2006 <l24020272@veracruz.tecnm.mx>
2026-04-09 17:06:02 -0600

    act de bd

--- bd.py
+++ bd.py
@@ -1,3 +1,40 @@
+import sqlite3
+
+conexion = sqlite3.connect("BaseDeDatos.db")
+cursor = conexion.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS Correos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    correo TEXT UNIQUE NOT NULL,
+    contraseña TEXT NOT NULL,
+    nombre TEXT
+)
+               
+""")
+
+conexion.commit()
+print("Base de datos inicializada correctamente.")
+
+
+def registrar(correo, contraseña, nombre):
+    conexion = sqlite3.connect("BaseDeDatos.db")
+    cursor = conexion.cursor()
+
+    try:
+        cursor.execute("""
+        INSERT INTO Correos (correo, contraseña, nombre)
+        VALUES (?, ?, ?)
+        """, (correo, contraseña, nombre))
+
+        conexion.commit()
+        print("Usuario registrado correctamente ")
+
+    except sqlite3.IntegrityError:
+        print("Ese correo ya está registrado en la base de datos.")
+
+    conexion.close()
+
 def login(correo, contraseña):
     conexion = sqlite3.connect("BaseDeDatos.db")  # MISMA BD
     cursor = conexion.cursor()
@@ -19,3 +56,9 @@ def login(correo, contraseña):
 
     conexion.close()
     return None
+
+registrar("inge@gmail.com", "1234", "Inge")
+
+user_id = login("inge@gmail.com", "1234")
+
+print("ID del usuario:", user_id)
