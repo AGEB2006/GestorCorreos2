cf78b91cf82474afbb52e60fd56395cebb60e660
DmcDante115 <l24020567@veracruz.tecnm.mx>
2026-04-08 19:15:57 -0600

    Actu 3

--- UI.py
+++ UI.py
@@ -6,17 +6,29 @@ Ventana.geometry("1200x700+250+50")
 
 Pilar = CTkFrame(Ventana,  fg_color="#2B2B2B", corner_radius=0)
 Pilar.place( x = 0, y = 40, relwidth=0.105, relheight=1)
-Fila = CTkFrame(Ventana, width=3000, height=120,  fg_color="#2B2B2B", corner_radius=0)
+Fila = CTkFrame(Ventana, width=2000, height=100,  fg_color="#2B2B2B", corner_radius=0)
 Fila.place( x = 0, y = 0, relwidth=1)
 Msj= CTkFrame (Ventana, fg_color = "blue", corner_radius=0)
-Msj.place( x = 126, y = 120, relwidth=0.985, relheight=1)
+Msj.place( x = 126, y = 100, relwidth=0.985, relheight=1)
+#---------------------------------------------------------------------------------------------------
+Enviar = CTkImage(light_image=Image.open("avion2.png"),
+                       dark_image=Image.open("avion2.png"),
+                       size=(70, 50))
+Boton_Enviar = CTkButton(Ventana, text="", image=Enviar, fg_color="#2B2B2B",hover_color="#3B3B3B", corner_radius=0,width=0,
+    height=0)
+Boton_Enviar.place(x=32, y=100)
+tooltip = CTkLabel(Ventana, text="Enviar", fg_color="#2B2B2B", text_color="white", corner_radius=6)
 
-Enviar = CTkImage(light_image=Image.open("tu_imagen.png"),
-                       dark_image=Image.open("tu_imagen.png"),
-                       size=(30, 30))
-
-
-
+def mostrar_tooltip(event):
+    x = Boton_Enviar.winfo_rootx() - Ventana.winfo_rootx()
+    y = Boton_Enviar.winfo_rooty() - Ventana.winfo_rooty()
+    tooltip.place(x=x + Boton_Enviar.winfo_width()//2 - 10,  
+                  y=y + Boton_Enviar.winfo_height() + -40)     
+def ocultar_tooltip(event):
+    tooltip.place_forget()
+Boton_Enviar.bind("<Enter>", mostrar_tooltip)
+Boton_Enviar.bind("<Leave>", ocultar_tooltip)
+#----------------------------------------------------------------------------------
 
 
 Ventana.mainloop()
